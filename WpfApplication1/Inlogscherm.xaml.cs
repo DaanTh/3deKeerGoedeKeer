86c7fb14f39ba6f4348b83a16ae1d793f75f5c08
Daan <Daantj3uh@hotmail.com>
2015-04-06 20:34:23 +0200

    volledige dinges eindelijk

---
+++ WpfApplication1/Inlogscherm.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+//Author : Daan Theunis
+//Date : 03.04.2015 18:56
+namespace ProjectChallenge
+{
+
+    public partial class Window1 : Window
+    {
+        private string[] gegevens = new string[2];
+        private char[] seperator = { ',' };
+        private string naam;
+        private string wachtwoord;
+        private string naamFile;
+        private string wachtwoordFile;
+
+        public Window1()
+        {
+            InitializeComponent();
+            naam = "";
+            wachtwoord = "";
+            naamFile = "";
+            wachtwoordFile = "";
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            wachtwoord = tbWachtwoord.Password;
+            naam = tbInlog.Text;
+            controleerGegevens();
+        }
+
+        private void controleerGegevens()
+        {
+            StreamReader lezer = File.OpenText("Aanmeldgegevens.txt");
+            string line = lezer.ReadLine();
+            gegevens = line.Split(seperator);
+            naamFile = gegevens[0].Trim();
+            wachtwoordFile = gegevens[1].Trim();
+
+            if (naam != naamFile)
+            {
+                errormessage.Text = "Foutieve gebruikersnaam";
+                lezer.Close();
+            }
+            else if (wachtwoord == wachtwoordFile)
+            {
+                Window2 leerstof = new Window2();
+                leerstof.Show();
+                this.Close();
+            }
+            else
+            {
+                errormessage.Text = "Foutief wachtwoord";
+                lezer.Close();
+            }
+
+
+
+        }
+    }
+}
